989323ebfd6d149d96197e22b9368ef0dd3402db
Max Gripe <108034506+MaxGripe@users.noreply.github.com>
2024-10-07 01:51:33 +0200

    yolo

--- Program.fs
+++ Program.fs
@@ -1,24 +1,101 @@
 ﻿open System
-open NAudio.Wave
+
+let clearLines () =
+    Console.SetCursorPosition(0, Console.CursorTop - 1)
+    Console.Write(new string (' ', Console.WindowWidth - 1))
+    Console.SetCursorPosition(0, Console.CursorTop)
+
+let availableCommands = "Commands: [0..100] for volume, [q] to quit: "
+let urlPrompt = "Please enter stream address (in http:// format) or [q] to quit: "
+
+let rec awaitingUrl () =
+    let url = Console.ReadLine()
+    clearLines ()
+
+    match url with
+    | "q" -> exit 0
+    | s when
+        s.StartsWith("http://")
+        || s.StartsWith("https://")
+        ->
+        playStream (url)
+    | _ ->
+        clearLines ()
+        Console.WriteLine("Last command entered: {0}", url)
+        Console.Write(urlPrompt)
+        awaitingUrl ()
+
+and playStream (url: string) =
+    try
+        use reader = new NAudio.Wave.MediaFoundationReader(url)
+        use waveOut = new NAudio.Wave.WaveOutEvent()
+        waveOut.Init(reader)
+
+        waveOut.PlaybackStopped.Add (fun args ->
+            clearLines ()
+            Console.WriteLine("")
+            clearLines ()
+            Console.WriteLine("Playback has ended :(")
+            exit 0)
+
+        waveOut.Play()
+
+        clearLines ()
+        Console.WriteLine("Playing: {0}", url)
+        Console.Write(availableCommands)
+
+        let rec processCommands () =
+            let input = Console.ReadLine()
+            clearLines ()
+
+            match input with
+            | "q" -> exit 0
+            | s ->
+                match Int32.TryParse(s) with
+                | (true, vol) when vol >= 0 && vol <= 100 ->
+                    let volume = float32 vol / 100.0f
+                    waveOut.Volume <- volume
+
+                    clearLines ()
+                    Console.WriteLine("Playing: {0}, Volume: {1}%", url, vol)
+                    Console.Write(availableCommands)
+                    processCommands ()
+                | _ ->
+                    clearLines ()
+                    Console.WriteLine("Last command entered: {0}", input)
+                    Console.Write(availableCommands)
+                    processCommands ()
+
+        processCommands ()
+
+    with
+    | ex ->
+        clearLines ()
+        Console.WriteLine("Error: {0}", ex.Message)
+        Console.Write(urlPrompt)
+        awaitingUrl ()
 
 [<EntryPoint>]
 let main argv =
-    Console.Write("Please enter stream address: ")
-    let url = Console.ReadLine()
+    System.Console.Clear()
+    Console.WriteLine("╔══════════════════════════════════════════════════════════════════════════════╗")
+    Console.WriteLine("║         ░░░░░░  ▄█▒░░     ▄▄█▀▀   ▄▄█▓▓▓▄ ░░░░░    ▄▓▓▓▓▓▓▓▓█▀    ░░░░░     ░║")
+    Console.WriteLine("║        ░░░░░  ▄█▓▒▒    ▄▄█▓▒    ▄█▓▓▒▒▒▓▓▄ ░░   ░▓▓▓▓▓▓▓▓▓░░    ░░░░░  ▓▓█  ░║")
+    Console.WriteLine("║       ░░░░  ▄▓▓▒▒   ▄▄█▓▓▒░   ▄▓▓▓▓▒░░░░▓▓▄   ░░▓▓▓▒░░░░░░░    ░░░░  ▄▓▓▓█  ░║")
+    Console.WriteLine("║      ░░░░  ▓▒▒░   ▒▓▓▓     ▄█▓▓▓▒▒░    ░░▓▓█  ░░░░░    ░░░   ░░░░  ▄▓▓▓▓▓   ░║")
+    Console.WriteLine("║     ░░░  ░▓▓▒░ ▒▒▓▓▓▒    ░░▒▓▓▓▒░        ░▓▓░  ░░   ░░▒▒░   ░░░   ▓▓▓▓▓▓█  ░░║")
+    Console.WriteLine("║    ░░░  ▒▓▓▒░  ▒▓▓▒░   ░░▒▓▓▓▒░     ░░░   ░▓▓░     ░▒▒▓▓   ░░░  ▓▓▓▓▓▓▓█  ░░░║")
+    Console.WriteLine("║   ░░   ▒▓▓▓▒░   ░░▓▓▒▒   ░░▓▓▒    ░░░░░░░  ░▓░░    ▒▒▓▓    ░  ░▓▓▓▓▓▓▓▓▀  ░░ ║")
+    Console.WriteLine("║  ░░  ▒▒▓▓▓▒▒░     ░░▓▒▒░   ░▓▓▓    ░░░░░  ░▓▓░   ░▒▒▓▓      ░▓▓▓▓▒░░    ░░░  ║")
+    Console.WriteLine("║ ░░  ▒▓▓▓▓▓▒▒░  ░░  ░░▓▓▓▓    ░▒▓▒   ░░   ▓▓▓░   ░▒▓▓▓     ░░▓▓▓▓▒░   ░░░░░   ║")
+    Console.WriteLine("║░░  ▒▓▓▓▒▒░░░░  ░░░░  ░░▓▓▓░    ▒▒▒░    ▓▓▒▒   ░░▒▓▓░    ░░▓▓▓▒░░   ░░░░░░    ║")
+    Console.WriteLine("║░  ▒▒▒▒▒░░      ░░░░░░  ░▒▒▓▓░    ░▒▒▒░░▒▒▒   ░░▒▓▓░     ░▓▓▓░░   ░░░░░░░     ║")
+    Console.WriteLine("║░ ░▒░░        ░░░░░░░░░  ░▒▒▓░░    ░▒░░░░    ░▒▓▓░░    ░▓▓░░  ░░░░░░░░░░      ║")
+    Console.WriteLine("║ ░░░      ░░░░░░░░░░░░░░░   ░░▒▒░░    ░░░    ░▒▓▓░   ░░ ░▒░  ░░░░░░░░░░       ║")
+    Console.WriteLine("╚══════════════════════════════════════════════════════════════════════════════╝")
+    Console.WriteLine("")
+    Console.Write("Please enter stream address or [q] to quit: ")
 
-    printfn "Now playing stream from %s" url
-
-    try
-        use reader = new MediaFoundationReader(url)
-        use waveOut = new WaveOutEvent()
-        waveOut.Init(reader)
-        waveOut.Play()
-
-        Console.WriteLine("Press any key to stop playback...")
-        Console.ReadKey() |> ignore
-
-        waveOut.Stop()
-    with
-    | ex -> Console.WriteLine("An error occurred: {0}", ex.Message)
+    awaitingUrl ()
 
     0
--- README.md
+++ README.md
@@ -5,20 +5,35 @@
 ![GitHub forks](https://img.shields.io/github/forks/MaxGripe/repository-template)
 ![GitHub Repo stars](https://img.shields.io/github/stars/MaxGripe/repository-template)
 
-
 # Koji Stream Player
 
+![Koji](gfx/koji.png)
+
 A simple console-based program to play audio streams.
-This is experimental, initial commit. Not ready for use. Please wait for a first release.
 
 ## Features
 
 - Lightweight and easy to use
+- Volume control
 - Console-based
+
+![Koji](gfx/terminal.png)
 
 ## Getting started
 
-Run the executable and provide the URL of the stream you want to play.
+Run the executable and provide the URL of the stream you want to play, for example:
+
+`https://ice1.somafm.com/spacestation-128-mp3`
+
+or
+
+`http://ice.bassdrive.net:80/stream32`
+
+That's it!
+
+## Support
+
+If possible, please support the radio station you are listening to! :)
 
 ## License
 
